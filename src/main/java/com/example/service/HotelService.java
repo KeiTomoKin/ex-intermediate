54a6502f215af056c8dd8fdc4a9de1a9ac543b61
tomookakeita <keita516tomo@outlook.jp>
2019-05-31 15:42:04 +0900

    ホテル情報を操作するサービスを作成

---
+++ src/main/java/com/example/service/HotelService.java
@@ -0,0 +1,33 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Hotel;
+import com.example.repository.HotelRepository;
+
+/**
+ * ホテル情報を操作するサービス
+ * 
+ * @author keita.tomooka
+ *
+ */
+@Service
+@Transactional
+public class HotelService {
+	@Autowired	
+	private  HotelRepository hotelRepository;
+	
+	/**
+	 * 指定した金額よりも安いホテルの情報リストを取得する.
+	 * @param price 金額
+	 * @return　ホテル情報のリスト
+	 */
+	public List<Hotel> showList(Integer price){
+		return hotelRepository.findByPlice(price);
+	}
+
+}
